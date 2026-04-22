1b08f14b8f94091b30cc7f51f19b17a816dc85f3
Bartu <canhbartu@gmail.com>
2026-04-22 23:25:36 +0200

    Add naive solution

--- problems/problem_28.py
+++ problems/problem_28.py
@@ -1,8 +1,14 @@
 def strStr(haystack:str, needle:str) -> str:
+    
+    """
+    Given two strings needle and haystack,
+    return the index of the first occurrence of needle in haystack,
+    or -1 if needle is not part of haystack.
+    """
+    
+
     if needle not in haystack:
         return -1
     else:
         return haystack.find(needle)
     
-
-print(strStr("abc", "bc"))
